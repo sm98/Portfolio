07fe838f3916cb140bf530c4d8b1b7ed40c949d1
Sanoop Menon <sanoopmenon30@gmail.com>
2022-05-07 22:27:02 +0530

    copied old code content

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  
+	'use strict';
+
+	var lockscroll=false;
+
+	 var c, currentScrollTop = 0,
+		 navbar = $('#header');
+  
+	 $(window).scroll(function () {
+		var a = $(window).scrollTop();
+		var b = 56;
+	   
+		currentScrollTop = a;
+	   
+		if ((c < currentScrollTop )&&( a > b + b )&&(!lockscroll)) {
+		  navbar.addClass("scrollUp");
+		} else if (c > currentScrollTop && !(a <= b)) {
+		  navbar.removeClass("scrollUp");
+		}
+		c = currentScrollTop;
+	});
+	var body = document.body;
+	var $hamburger = $(".hamburger");
+  	$hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+	body.classList.toggle("lockscroll");
+  	});
+  });
