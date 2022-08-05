0defb39be8c5b4f9d2dd9b9aea8d0433ce077567
Sanoop Menon <sanoopmenon30@gmail.com>
2022-08-06 00:27:10 +0530

    Major update homepage

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-import Splide from '@splidejs/splide';
-
-
-new Splide( '.splide' ).mount();
-
 
 $(document).ready(function () {  
 	'use strict';
-	window.alert("sometext");
+	
 	var lockscroll=false;
 	 var c, currentScrollTop = 0,
 		 navbar = $('#header');
+
+
+	let mouseCursor = document.querySelector(".cursor");
+	let navLinks = document.querySelectorAll(".nav-links li");
+	let projLinks = document.querySelectorAll(".project-link");
+	console.log(navLinks);
+	console.log(projLinks);
+	
+
+	window.addEventListener('mousemove',cursor);
+	function cursor(e){
+		mouseCursor.style.top = e.pageY + 'px';
+		mouseCursor.style.left = e.pageX + 'px';
+	}
+
+	navLinks.forEach(link=>{
+		link.addEventListener('mouseover',()=>{
+			mouseCursor.classList.add("link-grow");
+			
+		});
+		link.addEventListener('mouseleave',()=>{
+			mouseCursor.classList.remove("link-grow");
+		});
+	});
+		
+	projLinks.forEach(link=>{
+		link.addEventListener('mouseover',()=>{
+			mouseCursor.classList.add("link-grow-large");
+			
+		});
+		link.addEventListener('mouseleave',()=>{
+			mouseCursor.classList.remove("link-grow-large");
+		});
+	});
+
+
+
+
 
 	 $(window).scroll(function () {
 		var a = $(window).scrollTop();
@@ -24,6 +57,44 @@ $(document).ready(function () {
 		  navbar.removeClass("scrollUp");
 		}
 		c = currentScrollTop;
+
+		
+		var topOfWindow = $(window).scrollTop();
+		var partOfWindow= $(window).height();
+		var bottom3rdOfWindow = topOfWindow + (partOfWindow*2/3);
+		var bottomOfWindow = topOfWindow + $(window).height();
+
+		var card1 =$('#card1').offset().top;
+		var card2 =$('#card2').offset().top;
+		var card3 =$('#card3').offset().top;
+		var card4 =$('#card4').offset().top;
+		var card5 =$('#card5').offset().top;
+		var card6 =$('#card6').offset().top;
+		if(bottom3rdOfWindow>card6){$("#bg").css("background-color", "#fef8f7");$("#header").css("background-color", "#fef8f7"); }
+		else{
+			if(bottom3rdOfWindow>card5){$("#bg").css("background-color", "#fef4f1");$("#header").css("background-color", "#fef4f1"); }
+			else{
+				if(bottom3rdOfWindow>card4){$("#bg").css("background-color", "#f6f7f1");$("#header").css("background-color", "#f6f7f1"); }
+				else{
+					if(bottom3rdOfWindow>card3){$("#bg").css("background-color", "#f6f7f4");$("#header").css("background-color", "#f6f7f4"); }
+					else{
+						if(bottom3rdOfWindow>card2){$("#bg").css("background-color", "#ebf9f4"); $("#header").css("background-color", "#ebf9f4");}
+						else{
+							if(bottom3rdOfWindow>card1){$("#bg").css("background-color", "#fee7ec"); $("#header").css("background-color", "#fee7ec");}
+							else{
+								$("#bg").css("background-color", "#ffffff");$("#header").css("background-color", "#ffffff");
+							}
+						}
+					}
+				}
+			}			
+		}
+		
+		
+
+		
+		
+
 	});
 	var body = document.body;
 	
